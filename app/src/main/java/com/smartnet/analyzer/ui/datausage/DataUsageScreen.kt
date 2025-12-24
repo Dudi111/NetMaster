9c279ab86820c7728c20bcd524bf30f06ead93e6
pdraj <pd.rajiv.000@gmail.com>
2025-12-24 19:37:58 +0530

    Added data usage logic

--- app/src/main/java/com/smartnet/analyzer/ui/datausage/DataUsageScreen.kt
+++ app/src/main/java/com/smartnet/analyzer/ui/datausage/DataUsageScreen.kt
@@ -1,26 +1,46 @@
 package com.smartnet.analyzer.ui.datausage
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.Icon
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.MaterialTheme
 
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import com.smartnet.analyzer.common.theme.DarkGradient
 import com.smartnet.analyzer.data.AppDataUsage
 import com.smartnet.analyzer.ui.datausage.viewmodel.DataUsageViewmodel
 
 @Composable
 fun DataUsageScreen(
-     dataUsageViewmodel: DataUsageViewmodel = hiltviewmodel()
+     dataUsageViewmodel: DataUsageViewmodel = hiltViewModel()
 ) {
 
     Column(
@@ -29,6 +49,7 @@ fun DataUsageScreen(
             .background(DarkGradient)
     ) {
 
+        Header()
         LazyColumn {
             items(dataUsageViewmodel.dataList.size) { item ->
             AppDetailsView(dataUsageViewmodel.dataList[item])
@@ -39,26 +60,60 @@ fun DataUsageScreen(
 
 @Composable
 fun AppDetailsView(dataUsage: AppDataUsage) {
-
+    Spacer(Modifier.height(20.dp))
     Row(
         modifier = Modifier.fillMaxSize()
+        .border(
+            width = 1.dp,
+    color = Color.Gray,
+    shape = RectangleShape
+    )
+    .padding(12.dp)
     ) {
         Image(
-            imageVector = dataUsage.icon,
+            bitmap = dataUsage.icon!!.toImageBitmap(),
             contentDescription = null,
+            modifier = Modifier.size(40.dp)
         )
-        Image
 
         Text(
-            text = dataUsage.packageName,
-            fontSize = 30.sp,
+            text = dataUsage.appName,
+            fontSize = 10.sp,
+            fontWeight = FontWeight.Bold
         )
+        Spacer(Modifier.size(15.dp))
+//        Text(
+//            text = dataUsage.packageName,
+//            fontSize = 30.sp,
+//        )
+        Spacer(Modifier.size(15.dp))
         Text(
-            text = dataUsage.usagePercentage,
-            fontSize = 30.sp,
+            text = dataUsage.totalBytes.toString(),
+            fontSize = 20.sp,
             modifier = Modifier.align(Alignment.Top)
         )
     }
+}
+
+@Composable
+fun Header() {
+    Text(
+        text = "DATA USAGE",
+        modifier = Modifier.padding(top = 52.dp, bottom = 16.dp),
+        style = MaterialTheme.typography.h6
+    )
+}
+
+fun Drawable.toImageBitmap(): ImageBitmap {
+    val bitmap = Bitmap.createBitmap(
+        intrinsicWidth.takeIf { it > 0 } ?: 1,
+        intrinsicHeight.takeIf { it > 0 } ?: 1,
+        Bitmap.Config.ARGB_8888
+    )
+    val canvas = Canvas(bitmap)
+    setBounds(0, 0, canvas.width, canvas.height)
+    draw(canvas)
+    return bitmap.asImageBitmap()
 }
 
 
--- app/src/main/java/com/smartnet/analyzer/ui/datausage/viewmodel/DataUsageViewmodel.kt
+++ app/src/main/java/com/smartnet/analyzer/ui/datausage/viewmodel/DataUsageViewmodel.kt
@@ -1,10 +1,110 @@
 package com.smartnet.analyzer.ui.datausage.viewmodel
 
+import android.app.usage.NetworkStats
+import android.app.usage.NetworkStatsManager
+import android.content.Context
+import android.net.ConnectivityManager
+import android.telephony.TelephonyManager
+import android.util.Log
+import androidx.lifecycle.ViewModel
 import com.smartnet.analyzer.data.AppDataUsage
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
+import java.util.Calendar
+import javax.inject.Inject
 
 
 @HiltViewModel
-class DataUsageViewmodel() {
+class DataUsageViewmodel @Inject constructor(
+    @ApplicationContext context: Context
+) : ViewModel(){
 
-    val dataList = mutableListOf(AppDataUsage())
+    var dataList = mutableListOf(AppDataUsage(icon = null, txBytes = 0L, rxBytes = 0L))
+
+    init {
+        dataList = getTodayAppWiseMobileDataUsage(context).toMutableList()
+    }
+
+    fun getTodayAppWiseMobileDataUsage(context: Context): List<AppDataUsage> {
+
+        val networkStatsManager =
+            context.getSystemService(Context.NETWORK_STATS_SERVICE) as NetworkStatsManager
+
+        val telephonyManager =
+            context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+
+        val packageManager = context.packageManager
+        val (startTime, endTime) = getTodayStartEndMillis()
+
+        val usageMap = mutableMapOf<Int, Pair<Long, Long>>() // uid → rx, tx
+
+        val stats = networkStatsManager.querySummary(
+            ConnectivityManager.TYPE_MOBILE,
+            null,
+            startTime,
+            endTime
+        )
+
+        val bucket = NetworkStats.Bucket()
+        while (stats.hasNextBucket()) {
+            stats.getNextBucket(bucket)
+            val uid = bucket.uid
+            Log.d("dudi","App uid: $uid")
+            val prev = usageMap[uid] ?: (0L to 0L)
+            usageMap[uid] = Pair(
+                prev.first + bucket.rxBytes,
+                prev.second + bucket.txBytes
+            )
+        }
+        stats.close()
+
+        val appList = mutableListOf<AppDataUsage>()
+
+        usageMap.forEach { (uid, bytes) ->
+            try {
+                val packages = packageManager.getPackagesForUid(uid) ?: return@forEach
+                for (pkg in packages) {
+                    val appInfo = packageManager.getApplicationInfo(pkg, 0)
+                    val appName = packageManager.getApplicationLabel(appInfo).toString()
+                    val icon = packageManager.getApplicationIcon(appInfo)
+                    Log.d("dudi","app info: $appInfo , name: $appName , and icon: $icon")
+
+                    appList.add(
+                        AppDataUsage(
+                            packageName = pkg,
+                            appName = appName,
+                            icon = icon,
+                            rxBytes = bytes.first,
+                            txBytes = bytes.second
+                        )
+                    )
+                }
+            } catch (e: Exception) {
+                Log.d("dudi","Error while getting names: $e")
+            }
+        }
+
+        return appList.sortedByDescending { it.totalBytes }
+    }
+
+    fun getTodayStartEndMillis(): Pair<Long, Long> {
+        val cal = Calendar.getInstance().apply {
+            set(Calendar.HOUR_OF_DAY, 0)
+            set(Calendar.MINUTE, 0)
+            set(Calendar.SECOND, 0)
+            set(Calendar.MILLISECOND, 0)
+        }
+        val start = cal.timeInMillis
+
+        cal.apply {
+            set(Calendar.HOUR_OF_DAY, 23)
+            set(Calendar.MINUTE, 59)
+            set(Calendar.SECOND, 59)
+            set(Calendar.MILLISECOND, 999)
+        }
+        val end = cal.timeInMillis
+
+        return start to end
+    }
+
 }

3c7d74c95df36d5f3f5470f38ff3a023ae2cf789
pdraj <pd.rajiv.000@gmail.com>
2026-01-17 17:27:45 +0530

    updated code

--- app/src/main/java/com/smartnet/analyzer/data/DataUsageHelper.kt
+++ app/src/main/java/com/smartnet/analyzer/data/DataUsageHelper.kt
@@ -4,10 +4,8 @@ import android.app.usage.NetworkStats
 import android.app.usage.NetworkStatsManager
 import android.content.Context
 import android.content.pm.PackageManager
-import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.os.RemoteException
-import android.util.Log
 import androidx.core.content.getSystemService
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
@@ -19,64 +17,6 @@ class DataUsageHelper @Inject constructor(
 
     private var totalDeviceRx = 0L
     private var totalDeviceTx = 0L
-
-    fun getTodayAppWiseMobileDataUsage(context: Context): List<AppDataUsage> {
-
-        val networkStatsManager =
-            context.getSystemService(Context.NETWORK_STATS_SERVICE) as NetworkStatsManager
-
-        val packageManager = context.packageManager
-        val (startTime, endTime) =
-        val usageMap = mutableMapOf<Int, Pair<Long, Long>>() // uid → rx, tx
-
-        val stats = networkStatsManager.querySummary(
-            ConnectivityManager.TYPE_MOBILE,
-            null,
-            startTime,
-            endTime
-        )
-
-        val bucket = NetworkStats.Bucket()
-        while (stats.hasNextBucket()) {
-            stats.getNextBucket(bucket)
-            val uid = bucket.uid
-            Log.d("dudi","App uid: $uid")
-            val prev = usageMap[uid] ?: (0L to 0L)
-            usageMap[uid] = Pair(
-                prev.first + bucket.rxBytes,
-                prev.second + bucket.txBytes
-            )
-        }
-        stats.close()
-
-        val appList = mutableListOf<AppDataUsage>()
-
-        usageMap.forEach { (uid, bytes) ->
-            try {
-                val packages = packageManager.getPackagesForUid(uid) ?: return@forEach
-                for (pkg in packages) {
-                    val appInfo = packageManager.getApplicationInfo(pkg, 0)
-                    val appName = packageManager.getApplicationLabel(appInfo).toString()
-                    val icon = packageManager.getApplicationIcon(appInfo)
-                    Log.d("dudi","app info: $appInfo , name: $appName , and icon: $icon")
-
-                    appList.add(
-                        AppDataUsage(
-                            packageName = pkg,
-                            appName = appName,
-                            icon = icon,
-                            rxBytes = bytes.first,
-                            txBytes = bytes.second
-                        )
-                    )
-                }
-            } catch (e: Exception) {
-                Log.d("dudi","Error while getting names: $e")
-            }
-        }
-
-        return appList.sortedByDescending { it.totalBytes }
-    }
 
     private fun getAppDataUsage(
         startTime: Long,
